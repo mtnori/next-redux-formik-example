baf0fbf789ba0ec174bd911b87eb60f927b09d17
mtnori <moto44.noriyuki@gmail.com>
2018-04-11 21:52:13 +0900

    add async Test Page

--- pages/page.js
+++ pages/page.js
@@ -3,13 +3,18 @@ import React from 'react';
 import withRedux from 'next-redux-wrapper';
 import { initStore, fooAction } from '../redux/store';
 import MyForm from '../components/MyForm';
+import Router from 'next/router';
 
 class Page extends React.Component {
+  handleClick = () => {
+    Router.push('/async');
+  }
   render() {
     const { foo, fooAction } = this.props;
     return (
       <div>
         <button onClick={fooAction}>Click</button>
+        <button onClick={this.handleClick}>ToAsyncPage</button>
         <p>{foo}</p>
         <MyForm foo={foo} />
       </div>
